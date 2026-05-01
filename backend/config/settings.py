8ad67218921ff123294d0e3a8f14730f8bb53ffc
salmakhalill <salmakhalil283@gmail.com>
2026-05-01 09:45:22 +0300

    refactor(core): rename main project directory to config for better modularity

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'scanner_project.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'scanner_project.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
